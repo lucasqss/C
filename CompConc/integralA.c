9d6ca6bcf598bb464a3682aa61c2dd8c6fa2bbdf
LucasQSS <31527218+LucasQSS@users.noreply.github.com>
2019-10-29 19:31:38 -0300

    Add files via upload

---
+++ CompConc/integralA.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+double func(double x){
+    return (1+x);
+}
+double integral(double a, double b, int n){
+    double h = (b-a)/n;
+    double resultado = 0;
+    for(int i=0; i<n; i++){
+        resultado += h*func((a + h/2) + i*h);
+    }
+    return resultado;
+}
+int main()
+{
+    double a, b, erro;
+    int n;
+    printf("intervalo [a, b]:\n");
+    printf("a:\n");
+    scanf("%lf", &a);
+    printf("b:\n");
+    scanf("%lf", &b);
+    printf("erro maximo\n");
+    scanf("%lf", &erro);
+    n = ceil(sqrt(1/erro));
+    printf("\n \n resultado: %lf", integral(a, b, n));
+    
+
+
+
+    
+
+    return 0;
+}
+
